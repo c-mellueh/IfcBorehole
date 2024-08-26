db4944cf96f8bb5c62ab918ad100ac4a86898a20
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-08-26 16:17:31 +0200

    add logging to main

--- boreholeCreator/core/main.py
+++ boreholeCreator/core/main.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Type
 
 from boreholeCreator import tool
@@ -7,10 +8,22 @@ def create_file(output_path, ifc: Type[tool.Ifc], location: Type[tool.Location],
                 stratum: Type[tool.Stratum], styling: Type[tool.Styling]) -> None:
     if not (borehole.is_dataframe_filled() and stratum.is_dataframe_filled()):
         return
+    logging.info("Create IFC-Template")
     file = ifc.get_ifcfile()
     site_placement = location.get_site_placement()
+    logging.info("Create IfcSite")
+
     ifc.create_ifc_site(site_placement)
+    logging.info("Create Boreholes")
+
     boreholes = borehole.create_boreholes()
+    logging.info("Create Boreholes to Site")
+
     ifc.assign_entities_to_site(boreholes)
+    logging.info("Style Entities Boreholes to Site")
+
     styling.style_entities()
+    logging.info("Write Ifc-File")
+
     file.write(output_path)
+    logging.info("Done!")
